84bd11954733405b7e155f63f1a85e38fca3c2f9
Scott Doyen <srdoyen@gmail.com>
2020-08-07 16:47:15 -0700

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Import the React and ReactDOM Libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Create a react component
+
+// Take the react component and show it on the screen
